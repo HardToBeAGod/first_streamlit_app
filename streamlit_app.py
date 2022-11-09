14720f4374816f5796914db5613e5debf655f692
Max Pol <33985758+HardToBeAGod@users.noreply.github.com>
2022-11-09 12:24:46 +0100

    Update streamlit_app.py
    code housekeeping

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error impront URLError
 
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatlmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -10,8 +13,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-
+# import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -27,7 +29,7 @@ streamlit.header('Fruityvise Fruit Advise!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+# import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
@@ -35,8 +37,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output in the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+# don't run anything past here while we troubleshoot
+streamlit.stop
 
+# Creating a snowflake connection and execute query
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -48,5 +52,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ' + add_my_fruit)
 
-#Inserting new fruits from the user input to Snowflake fruit_load_table
+# Inserting new fruits from the user input to Snowflake fruit_load_table
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
