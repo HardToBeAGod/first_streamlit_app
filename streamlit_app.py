197157402b8510e05c92f57b3456ab29d8f2b2c6
Max Pol <33985758+HardToBeAGod@users.noreply.github.com>
2022-11-09 12:27:45 +0100

    Update streamlit_app.py
    missing parantheses for streamlit.stop command

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
-streamlit.stop
+streamlit.stop()
 
 # Creating a snowflake connection and execute query
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
